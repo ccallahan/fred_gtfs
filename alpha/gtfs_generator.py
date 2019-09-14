4117b790d485117269567bd6ef8ec1f07558fe58
Chance Callahan <chance1callahan@gmail.com>
2019-09-14 02:44:31 -0400

    Added more stops.

--- alpha/gtfs_generator.py
+++ alpha/gtfs_generator.py
@@ -23,14 +23,14 @@ service_period.SetDateHasService("20191225", has_service=False) # Christmas Day
 
 # Stafford County Runs
 
-route_d1 = schedule.AddRoute(short_name="D1", long_name="Train Station to Olde Forge")
-route_d2 = schedule.AddRoute(short_name="D2", long_name="FRED Central, Olde Forge, England Run, GEICO")
-route_d3 = schedule.AddRoute(short_name="D3", long_name="Stafford Shopping Loop")
-route_d4 = schedule.AddRoute(short_name="D4", long_name="Stafford Marketplace to Northampton Blvd.")
-route_d5 = schedule.AddRoute(short_name="D5", long_name="FRED Central to Stafford Courthouse")
-route_d6 = schedule.AddRoute(short_name="D6", long_name="Stafford County EXPRESS")
+route_d1 = schedule.AddRoute(short_name="D1", long_name="Train Station to Olde Forge", route_type="Bus", route_id="D1")
+route_d2 = schedule.AddRoute(short_name="D2", long_name="FRED Central, Olde Forge, England Run, GEICO", route_type="Bus", route_id="D2")
+route_d3 = schedule.AddRoute(short_name="D3", long_name="Stafford Shopping Loop", route_type="Bus", route_id="D3")
+route_d4 = schedule.AddRoute(short_name="D4", long_name="Stafford Marketplace to Northampton Blvd.", route_type="Bus", route_id="D4")
+route_d5 = schedule.AddRoute(short_name="D5", long_name="FRED Central to Stafford Courthouse", route_type="Bus", route_id="D5")
+route_d6 = schedule.AddRoute(short_name="D6", long_name="Stafford County EXPRESS", route_type="Bus", route_id="D6")
 
-# Stafford Routes
+# Bus Stops
 
 stop_6 = schedule.AddStop(lat="38.298318", lng="-77.456132", name="Train Station (Caroline St.)", stop_id="6")
 stop_67 = schedule.AddStop(lat="38.29797", lng="-77.457208", name="Train Station (Princess Anne St.)", stop_id="67")
@@ -40,3 +40,12 @@ stop_418 = schedule.AddStop(stop_id="418", name="Ferry Farm Shopping Center", la
 stop_291 = schedule.AddStop(stop_id="291", name="Rappahannock Optical", lat="38.305928", lng="-77.452731")
 stop_501 = schedule.AddStop(stop_id="501", name="Earl's True Value Hardware", lat="38.310067", lng="-77.448175")
 stop_432 = schedule.AddStop(stop_id="432", name="Chatham Heights and Marion Rd", lat="38.313291", lng="-77.450801")
+stop_450 = schedule.AddStop(stop_id="450", name="Old Forge Dr. and Warrenton Rd.", lat="38.337279", lng="-77.485423")
+stop_451 = schedule.AddStop(stop_id="451", name="Old Forge Dr. and Warrenton Rd.", lat="38.337167", lng="-77.485917")
+stop_449 = schedule.AddStop(stop_id="449", name="Warrenton and Quarels Rd", lat="38.33553", lng="-77.482431")
+stop_437 = schedule.AddStop(stop_id="437", name="Bulter Rd and YMCA", lat="38.317363", lng="-77.456062")
+stop_436 = schedule.AddStop(stop_id="436", name="Deacon Rd. @ Woodland Shopping Cntr.", lat="38.327251", lng="-77.439217")
+stop_435 = schedule.AddStop(stop_id="435", name="Culpeper St. and Greenwood Dr.", lat="38.32585", lng="-77.432881")
+stop_433 = schedule.AddStop(stop_id="433", name="Sherwood Dr. and Culpeper St.", lat="38.31813", lng="-77.432036")
+stop_431 = schedule.AddStop(stop_id="431", name="Sherwood Dr. and Adam Ct.", lat="38.317954", lng="-77.430227")
+stop_429 = schedule.AddStop(stop_id="429", name="Sherwood and Edwards Dr.", lat="38.317365", lng="-77.425359")
